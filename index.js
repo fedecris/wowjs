b4705cc7c8acde8ebe32f761a2aeac56c848ffd8
Federico Cristina <federicocristina@gmail.com>
2020-08-21 14:42:43 -0300

    Fix problema req.body en HTTP Post

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const IMDBParser = require("./parser/IMDBParser");
 const app = express();
+
+// create application/json parser
+const jsonParser = bodyParser.json();
+// create application/x-www-form-urlencoded parser
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 async function fetchSites(url) {
   let imdb = new IMDBParser(url);
@@ -8,10 +14,26 @@ async function fetchSites(url) {
   console.log(imdb.publicScore);
 }
 
-const url = process.argv[2];
-console.log(`Crawling ${url}...`);
-fetchSites(url);
+// ============ Pruebas invocacion desde terminal =========
+// const url = process.argv[2];
+// console.log(`Crawling ${url}...`);
+// fetchSites(url);
 
 // Escuchar
-// const port = process.env.PORT || 3000;
-//app.listen(port, () => console.log(`Listening on ${port}`));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on ${port}`));
+
+// Inicio
+app.get("/search", (req, res) => {
+  res.sendFile(`${__dirname}/site/search.html`);
+});
+
+// Resultados
+app.post("/search", urlencodedParser, (req, res) => {
+  res.send(`${req.body.title} (${req.body.year})`);
+});
+
+// Ejemplo json === POST /api/users gets JSON bodies ===
+// app.post('/api/users', jsonParser, function (req, res) {
+//     // create user in req.body
+//   })
